38534b43e0f3bc130cc6c78b0ea943768fc65d27
NekoAndLoli <vx1245399319@gmail.com>
2018-01-29 16:05:49 +0100

    nyan have been

--- libraries/RGB/RGB.h
+++ libraries/RGB/RGB.h
@@ -22,12 +22,12 @@ class RGB {
     byte previous[3] = {0, 0, 0};
 
     byte black[3]  = { 0, 0, 0 };
-    byte white[3]  = { 100, 100, 100 };
-    byte red[3]    = { 100, 0, 0 };
-    byte green[3]  = { 0, 100, 0 };
-    byte blue[3]   = { 0, 0, 100 };
-    byte yellow[3] = { 40, 95, 0 };
-    byte dimWhite[3] = { 30, 30, 30 };
+    byte white[3]  = { 30000, 30000, 30000 };
+    byte red[3]    = { 30000, 0, 0 };
+    byte green[3]  = { 0, 30000, 0 };
+    byte blue[3]   = { 0, 0, 30000 };
+    byte yellow[3] = { 10000, 30000, 0 };
+    byte dimWhite[3] = { 10000, 10000, 10000 };
 };
 
 RGB::RGB(byte pinr, byte ping, byte pinb) {
@@ -48,39 +48,5 @@ void RGB::refresh() {
   set(actual[0], actual[1], actual[2]);
 }
 
-void RGB::crossFade() {
-  crossFade(red);
-  crossFade(green);
-  crossFade(blue);
-  crossFade(yellow);
-}
-
-void RGB::crossFade(byte color[3]) {
-  for (int i = 0; i < 3; i++) color[i]=(color[i] * 255) / 100;
-  int step[3];
-  for (int i = 0; i < 3; i++) step[i] = calculateStep(previous[i], color[i]);
-  for (int i = 0; i <= 1020; i++) {
-    for (int j = 0; j < 3; j++) actual[j] = calculateVal(step[j], actual[j], i);
-    refresh();
-    delay(WAIT);
-  }
-  for (int i = 0; i < 3; i++) previous[i]=actual[i];
-}
-
-int RGB::calculateVal(int step, int val, int i) {
-  if (step && i % step == 0) {
-    if (step > 0) val += 1;
-    else if (step < 0) val -= 1;
-  }
-  if (val > 255) val = 255;
-  else if (val < 0) val = 0;
-  return val;
-}
-
-int RGB::calculateStep(int prevValue, int endValue) {
-  int step = endValue - prevValue; // What's the overall gap?
-  if (step) step = 1020 / step;
-  return step;
-}
 
 #endif
