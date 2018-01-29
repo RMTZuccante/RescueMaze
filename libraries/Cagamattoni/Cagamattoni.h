028ff3b8b3f1a6a9522b60337651e8bcec36383e
NekoAndLoli <vx1245399319@gmail.com>
2018-01-29 15:24:45 +0100

    Cagamattoni uploaded

---
+++ libraries/Cagamattoni/Cagamattoni.h
@@ -0,0 +1,69 @@
+#ifndef CAGAMATTONI_H
+#define CAGAMATTONI_H
+
+#include <Arduino.h>
+
+#define PIN1 6
+#define PIN2 7
+
+#define DELAY 869.56
+#define MINDEL 200
+
+class Cagamattoni {
+  public :
+    Cagamattoni(int ntappi);
+    void caga();
+    bool isEmpty();
+    void turnOut();
+    void turnIn();
+    void stop();
+    void begin();
+  private :
+    byte kits;
+};
+
+Cagamattoni::Cagamattoni(int ntappi) {
+  kits = ntappi;
+}
+void Cagamattoni::begin(){
+    pinMode(PIN1, OUTPUT);
+  pinMode(PIN2, OUTPUT);
+
+  digitalWrite(PIN1, 0);
+  digitalWrite(PIN2, 0);
+}
+void Cagamattoni::caga() {
+  digitalWrite(PIN2, 1);
+  delay(DELAY);
+  digitalWrite(PIN2, 0);
+  delay(MINDEL);
+  digitalWrite(PIN1, 1);
+  delay(MINDEL);
+  digitalWrite(PIN1, 0);
+  delay(MINDEL);
+  digitalWrite(PIN2, 1);
+  delay(MINDEL);
+  digitalWrite(PIN2, 0);
+  kits--;
+}
+
+bool Cagamattoni::isEmpty() {
+  return kits < 1;
+}
+
+void Cagamattoni::turnOut() {
+  digitalWrite(PIN2, 0);
+  digitalWrite(PIN1, 1);
+}
+
+void Cagamattoni::turnIn() {
+  digitalWrite(PIN1, 0);
+  digitalWrite(PIN2, 1);
+}
+
+void Cagamattoni::stop() {
+  digitalWrite(PIN1, 0);
+  digitalWrite(PIN2, 0);
+}
+
+#endif
---
+++ libraries/Cagamattoni/Cagamattoni.ino
@@ -0,0 +1,12 @@
+#include "Cagamattoni.h"
+Cagamattoni cagamattoni(6);
+void setup() {
+  cagamattoni.begin();
+}
+
+void loop() {
+    if (!cagamattoni.isEmpty())
+    cagamattoni.caga();
+    delay(2000);
+}
+
