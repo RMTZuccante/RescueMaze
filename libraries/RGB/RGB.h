51a9520824e738227b6ec4aab9333e7657df274b
NekoAndLoli <vx1245399319@gmail.com>
2018-01-29 15:56:07 +0100

    RGB nyaned!

--- libraries/RGB/RGB.h
+++ libraries/RGB/RGB.h
@@ -39,9 +39,9 @@ void RGB::begin(){
 	for (int i = 0; i < 3; i++) pinMode(pin[i], OUTPUT);
 }
 void RGB::set(byte red, byte green, byte blue) {
-  analogWrite(pin[0], red);
-  analogWrite(pin[1], green);
-  analogWrite(pin[2], blue);
+  pwmWrite(pin[0], red);
+  pwmWrite(pin[1], green);
+  pwmWrite(pin[2], blue);
 }
 
 void RGB::refresh() {
