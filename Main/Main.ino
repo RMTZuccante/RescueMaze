e53c3f1c3cb2208db2450d0a1f8c47cd67d6eb16
Marco Fincato <marcofincato777@gmail.com>
2018-02-19 09:40:35 +0100

    Fixed addresses and moved definitions

--- Main/Main.ino
+++ Main/Main.ino
@@ -6,59 +6,37 @@
 #include "VL53L0X.h"
 //#include "Matrix.h"
 
-//Temperature adresses
-#define T_LEFT 0x5A
-#define T_RIGHT 0x5B
-
-//Laser addresses
-#define L_LEFT 0x29
-#define L_FRONT 0x2A
-#define L_RIGHT 0x2B
-//Laser pin
-#define LX_LEFT PA15
-#define LX_FRONT PA10
-
-//RGB
-#define RED 4
-#define GREEN 5
-#define BLUE 6
-//Directions
-#define FRONT 0
-#define RIGHT 1
-#define LEFT 2
-#define BACK 3
-//Misurements
-#define CENTRED 60 //distanza del robot rispetto al muro per essere centrato
-
 //creating objects
 Moviment mov(50000);
 Color color;
 Temperature tempL (T_LEFT);
 Temperature tempR (T_RIGHT);
-RGB led(RED , GREEN , BLUE);
+RGB led(RED, GREEN, BLUE);
 VL53L0X laser[3] = {VL53L0X(), VL53L0X(), VL53L0X()};
 //Matrix matrix;
 
 uint16_t dist[3];
 
 void getDist() {
-  for (int i = 0; i < 3; i++) {
-    dist[i] = laser[i].read();
-  }//for
+  for (int i = 0; i < 3; i++) dist[i] = laser[i].read();
 }//getDist
 
 void go() {
+  laser[0].start();
   mov.go();
   uint16_t end = endDist(laser[0].read());
   while ( (laser[0].read() > end) && (color.read() == 2) );
   mov.stop();
+  laser[0].stop();
 }//go
 
 void back() {
+  laser[0].start();
   mov.go(true);
   uint16_t end = endDist(laser[0].read()) + 300;
   while (laser[0].read() < end);
   mov.stop();
+  laser[0].stop();
 }
 
 uint16_t endDist(uint16_t distance) {
@@ -68,28 +46,29 @@ uint16_t endDist(uint16_t distance) {
 void setAddresses() {
   pinMode(LX_LEFT, OUTPUT);
   pinMode(LX_FRONT, OUTPUT);
-  laser[2].setAddress(L_RIGHT);
+  laser[1].setAddress(L_RIGHT);
   pinMode(LX_FRONT, INPUT);
   delay(10);
-  laser[1].setAddress(L_FRONT);
+  laser[0].setAddress(L_FRONT);
   pinMode(LX_LEFT, INPUT);
   delay(10);
-  laser[0].setAddress(L_LEFT);
+  laser[2].setAddress(L_LEFT);
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Wire.begin();
   Wire2.begin();
   setAddresses();
   color.begin();
   mov.begin();
-  for(int i = 0 ; i<3 ; i++)laser[i].begin();
+  for(int i = 0 ; i<3 ; i++) laser[i].begin();
 }//setup
 
 void loop() {
-  /*  getDist();
-      matrix._check(dist,tempL.read(),tempR.read(),color.read());
+	  getDist();
+      /*
+	  matrix._check(dist,tempL.read(),tempR.read(),color.read());
       if(matrix.isBlack()){
         back();
         matrix.back();
@@ -106,6 +85,7 @@ void loop() {
           mov.rotate(false)
           break ;
       }//switch
+	  */
       go();
   */
 }//loop
--- libraries/definitions.h
+++ libraries/definitions.h
@@ -4,4 +4,30 @@
 #include <Wire.h>
 extern TwoWire Wire2(2);
 
+//Temperature adresses
+#define T_LEFT 0x5A
+#define T_RIGHT 0x5B
+
+//Laser addresses
+#define L_LEFT 0x29
+#define L_FRONT 0x2A
+#define L_RIGHT 0x2B
+//Laser pin
+#define LX_LEFT PA15
+#define LX_FRONT PA10
+
+//RGB
+#define RED 4
+#define GREEN 5
+#define BLUE 6
+
+//Directions
+#define FRONT 0
+#define RIGHT 1
+#define LEFT 2
+#define BACK 3
+
+//Misurements
+#define CENTRED 60 //distanza del robot rispetto al muro per essere centrato
+
 #endif
