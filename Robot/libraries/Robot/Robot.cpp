ada5cdffed3da27884054fc507e1c27c6ee75c98
NekoAndLoli <vx1245399319@gmail.com>
2018-03-11 14:36:33 +0100

    Update Robot.cpp

--- Robot/libraries/Robot/Robot.cpp
+++ Robot/libraries/Robot/Robot.cpp
@@ -66,8 +66,6 @@ void Robot::setAddresses() {
 }
 
 uint16_t Robot::endDist(uint16_t distance) {
-  uint16_t Robot::endDist(uint16_t distance) {
   distance -= 200;
   return distance - ((distance) % 300) + CENTRED;
-}
 }
