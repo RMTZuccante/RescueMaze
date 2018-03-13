147b214be9ea4c83401e2d89d9213543da4c956d
NekoAndLoli <vx1245399319@gmail.com>
2018-03-13 17:34:03 +0100

    Wow! Here averything is working!

--- Robot/libraries/Robot/Moviment/Moviment.cpp
+++ Robot/libraries/Robot/Moviment/Moviment.cpp
@@ -20,30 +20,25 @@ bool Moviment::check() {
   return orientation.check();
 }
 
-void Moviment::climb(){
-  motorFR.start(50000, false);
-  motorFL.start(50000, false);
-  motorRR.start(20000, false);
-  motorRL.start(20000, false);
-}
 void Moviment::go() {
   go(true);
 }
 
 void Moviment::go(bool invert) {
-  //direzione = orientation.yaw();
+  direzione = orientation.yaw();
   motorFR.start(bound(speed, 65535), !invert);
   motorFL.start(bound(speed, 65535), !invert);
   motorRR.start(bound(speed, 65535), !invert);
   motorRL.start(bound(speed, 65535), !invert);
 }
 
-//void Moviment::straight(){
-//  motorFR.start(bound(speed + (direzione-orientation.yaw())*1000, 65535), false);
-//  motorFL.start(bound(speed - (direzione-orientation.yaw())*1000, 65535), false);
-//  motorRR.start(bound(speed + (direzione-orientation.yaw())*1000, 65535), false);
-//  motorRL.start(bound(speed - (direzione-orientation.yaw())*1000, 65535), false);
-//}
+void Moviment::straight(bool invert){
+  float now=orientation.yaw();
+  motorFR.start(bound(speed + (direzione - now)*100, 65535), !invert);
+  motorFL.start(bound(speed - (direzione - now)*100, 65535), !invert);
+  motorRR.start(bound(speed + (direzione - now)*100, 65535), !invert);
+  motorRL.start(bound(speed - (direzione - now)*100, 65535), !invert);
+}
 
 void Moviment::rotate() {
   rotate(false);
@@ -51,25 +46,10 @@ void Moviment::rotate() {
 
 void Moviment::rotate(bool invert) {
   orientation.start();
-  float end = endAngle(orientation.yaw(), invert); 
-  if (!invert) {
-    if (end > 270) {
-      rotationSpeed(invert, 0);
-      while (orientation.yaw() > 5) {};
-      delay(100);
-    }
-    while (orientation.yaw() > end) {
-      rotationSpeed(invert, end);
-    }
-    stop();
-    while (orientation.yaw() < end) {
-      rotationSpeed(!invert, end-10);
-      //delay(500);
-    }
-  }
-  else {
+  float end = endAngle(orientation.yaw(), invert);
+  if (invert) {
     if (end < 90) {
-      rotationSpeed(invert, 360);
+      rotationSpeed(invert, 0);
       while (orientation.yaw() < 355) {};
       delay(100);
     }
@@ -78,7 +58,21 @@ void Moviment::rotate(bool invert) {
     }
     stop();
     while (orientation.yaw() > end) {
-      rotationSpeed(!invert, end+10);
+      rotationSpeed(!invert, end);
+    }
+  }
+  else {
+    if (end < 270) {
+      rotationSpeed(!invert, 360);
+      while (orientation.yaw() > 5) {};
+      delay(100);
+    }
+    while (orientation.yaw() > end) {
+      rotationSpeed(!invert, end);
+    }
+    stop();
+    while (orientation.yaw() < end) {
+      rotationSpeed(invert, end);
     }
   }
   setK(0, 0);
@@ -93,7 +87,7 @@ void Moviment::stop() {
 }
 
 void Moviment::setSpeed(uint16_t speed) {
-  this->speed = bound((speed) , 65535);
+  this->speed = speed;
   motorFR.setSpeed(speed);
   motorFL.setSpeed(speed);
   motorRR.setSpeed(speed);
@@ -110,16 +104,13 @@ void Moviment::setK(int rightK, int leftK) {
 }
 
 float Moviment::endAngle(float angle, bool invert) {
-  float ris=angle;
   if (invert) {
-    ris += 90;
-    return (ris > 360) ? ris - 360 : ris;
+    angle += 90;
+    return (angle > 360) ? angle - 360 : angle;
   }
   else {
-    ris -= 90;
-    Serial.println("endangle");
-    Serial.println((ris < 0) ? ris + 360 : ris);
-    return (ris < 0) ? ris + 360 : ris;
+    angle -= 90;
+    return (angle < 0) ? angle + 360 : angle;
   }
 }
 
@@ -127,13 +118,17 @@ void Moviment::rotationSpeed(bool invert , float endRotation) {
   direzione = orientation.yaw();
   if (endRotation - direzione > 0) setK(FIRST_K + ((endRotation - direzione) * 200), SECOND_K + ((endRotation - direzione) * 200));
   else setK(SECOND_K + ((direzione - endRotation) * 200), FIRST_K + ((direzione - endRotation) * 200));
-  motorFR.start(bound((speed + kR) , 65535), invert);
-  motorFL.start(bound((speed + kL) , 65535), !invert);
-  motorRR.start(bound((speed + kR*RK) , 65535), invert);
-  motorRL.start(bound((speed + kL*RK) , 65535), !invert);
-  Serial.print(endRotation);
-  Serial.print("  ");
-  Serial.println(direzione);
+  motorFR.start(bound((speed + kR) , 65535), !invert);
+  motorFL.start(bound((speed + kL) , 65535), invert);
+  motorRR.start(bound((speed + kR) , 65535), !invert);
+  motorRL.start(bound((speed + kL) , 65535), invert);
+  //  Serial.print(endRotation);
+  //  Serial.print("  ");
+  //  Serial.print(direzione);
+  //  Serial.print(" R: ");
+  //  Serial.print(bound((kR) , 65535));
+  //  Serial.print(" L: ");
+  //  Serial.println(bound((kL) , 65535));
 }
 
 uint16_t Moviment::bound(uint32_t n, uint16_t max) {
