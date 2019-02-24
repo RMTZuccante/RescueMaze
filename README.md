25ca5c629f78fee1a0de902b4003b88c3bbc7c7b
Marco Fincato <marcofincato777@gmail.com>
2019-02-24 11:16:05 +0100

    New project structure
    Stupid Arduino IDE

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # RescueMaze
 
-The script "convert.bat" is used to organize the project structure. The Arduino IDE won't compile libraries in subfolders, so before working on the project we move everything to the main folder. Before pushing the tool is used to recover the structure, to keep everything neater.
+I gave up on trying to keep this project neat. Blaming Arduino IDE for this.
---
+++ RobotMain/Moviment.h
@@ -0,0 +1,49 @@
+#ifndef Moviment_h
+#define Moviment_h
+
+#include <Arduino.h>
+#include "definitions.h"
+#include "Motor.h"
+#include "Temperature.h"
+#include "IMU.h"
+
+class Moviment {
+  public:
+    Moviment (uint16_t speed, Temperature *tl, Temperature *tr);
+    void begin();
+    bool check();
+    void go();
+    void go (bool invert);
+    void impennati(uint16_t speed);    
+    void straight();
+    void endGo();
+    void rotate();
+    void climb(int k);
+    void rotation(bool invert);
+    int rotate (bool invert);
+    int rotate (bool invert, float angle);
+    void stop();
+    void setSpeed(uint16_t speed);
+    void setK(int rightK, int leftK);
+    float inclination();
+    void idle();
+  private:
+    uint16_t bound (uint32_t n, uint16_t max);
+    float endAngle(float angle, bool invert, float end);
+    void rotationSpeed(bool direction , float endRotation);
+    void rotationSpeed(uint16_t speed, bool invert);
+    Motor motorFR = Motor(FL_IN1, FL_IN2);
+    Motor motorFL = Motor(FR_IN1, FR_IN2);
+    Motor motorRR = Motor(RL_IN1, RL_IN2);
+    Motor motorRL = Motor(RR_IN1, RR_IN2);
+    IMU orientation;
+    Temperature *tright;
+    Temperature *tleft;
+    uint16_t speed;
+    int kR;
+    int kL;
+    float direzione;
+    float fill;
+};
+
+#endif
