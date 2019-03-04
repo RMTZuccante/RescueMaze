4fd7576b8e157e90171903177d774ccf73f57df2
Marco Fincato <marcofincato777@gmail.com>
2019-03-04 01:06:54 +0100

    Moved constants to enums

--- RobotMain/APDS9960.cpp
+++ RobotMain/APDS9960.cpp
@@ -5,31 +5,31 @@ void APDS9960::begin() {
     setMode(ALL, false);
 
     /* Set default values for ambient light and proximity registers */
-    wireWriteData8Bit(APDS9960_ATIME, DEFAULT_ATIME);
-    wireWriteData8Bit(APDS9960_WTIME, DEFAULT_WTIME);
-    wireWriteData8Bit(APDS9960_PPULSE, DEFAULT_PROX_PPULSE);
-    wireWriteData8Bit(APDS9960_POFFSET_UR, DEFAULT_POFFSET_UR);
-    wireWriteData8Bit(APDS9960_POFFSET_DL, DEFAULT_POFFSET_DL);
-    wireWriteData8Bit(APDS9960_CONFIG1, DEFAULT_CONFIG1);
+    wireWriteData8Bit(ATIME, DEFAULT_ATIME);
+    wireWriteData8Bit(WTIME, DEFAULT_WTIME);
+    wireWriteData8Bit(PPULSE, DEFAULT_PROX_PPULSE);
+    wireWriteData8Bit(POFFSET_UR, DEFAULT_POFFSET_UR);
+    wireWriteData8Bit(POFFSET_DL, DEFAULT_POFFSET_DL);
+    wireWriteData8Bit(CONFIG1, DEFAULT_CONFIG1);
     setProxIntLowThresh(DEFAULT_PILT);
     setProxIntHighThresh(DEFAULT_PIHT);
     setLightIntLowThreshold(DEFAULT_AILT);
     setLightIntHighThreshold(DEFAULT_AIHT);
-    wireWriteData8Bit(APDS9960_PERS, DEFAULT_PERS);
-    wireWriteData8Bit(APDS9960_CONFIG2, DEFAULT_CONFIG2);
-    wireWriteData8Bit(APDS9960_CONFIG3, DEFAULT_CONFIG3);
+    wireWriteData8Bit(PERS, DEFAULT_PERS);
+    wireWriteData8Bit(CONFIG2, DEFAULT_CONFIG2);
+    wireWriteData8Bit(CONFIG3, DEFAULT_CONFIG3);
 }
 
 bool APDS9960::check() {
-  uint8_t id = wireReadData8Bit(APDS9960_ID);
+  uint8_t id = wireReadData8Bit(ID);
   return I2C::check(&I2C_1, APDS9960_I2C_ADDR) && (id == APDS9960_ID_1 || id == APDS9960_ID_2);
 }
 
 uint8_t APDS9960::getMode() {
-    return wireReadData8Bit(APDS9960_ENABLE);
+    return wireReadData8Bit(ENABLE);
 }
 
-void APDS9960::setMode(uint8_t mode, uint8_t enable) {
+void APDS9960::setMode(Mode mode, uint8_t enable) {
     uint8_t reg_val = getMode();
     /* Change bit(s) in ENABLE register */
     enable = enable & 0x01;
@@ -37,12 +37,12 @@ void APDS9960::setMode(uint8_t mode, uint8_t enable) {
         if (enable) reg_val |= (1 << mode);
         else reg_val &= ~(1 << mode);
     } else if (mode == ALL) reg_val = (enable)?0x7F:0x00;
-    wireWriteData8Bit(APDS9960_ENABLE, reg_val);
+    wireWriteData8Bit(ENABLE, reg_val);
 }
 
 void APDS9960::enableLightSensor(bool interrupts) {
     /* Set default gain, interrupts, enable power, and enable sensor */
-    setAmbientLightGain(DEFAULT_AGAIN);
+    setAmbientLightGain(AG4X);
     setAmbientLightIntEnable(interrupts);
     enablePower();
     setMode(AMBIENT_LIGHT, 1);
@@ -55,8 +55,8 @@ void APDS9960::disableLightSensor() {
 
 void APDS9960::enableProximitySensor(bool interrupts) {
     /* Set default gain, LED, interrupts, enable power, and enable sensor */
-    setProximityGain(DEFAULT_PGAIN);
-    setLEDDrive(DEFAULT_LDRIVE);
+    setProximityGain(PG4X);
+    setLEDDrive(LD100MA);
     setProximityIntEnable(interrupts);
     enablePower();
     setMode(PROXIMITY, 1);
@@ -76,200 +76,195 @@ void APDS9960::disablePower() {
 }
 
 uint16_t APDS9960::readAmbientLight() {
-    return wireReadData16Bit(APDS9960_CDATAL);
+    return wireReadData16Bit(CDATA);
 }
 
 uint16_t APDS9960::readRedLight() {
-    return wireReadData16Bit(APDS9960_RDATAL);
+    return wireReadData16Bit(RDATA);
 }
 
 uint16_t APDS9960::readGreenLight() {
-    return wireReadData16Bit(APDS9960_GDATAL);
+    return wireReadData16Bit(GDATA);
 }
 
 uint16_t APDS9960::readBlueLight() {
-    return wireReadData16Bit(APDS9960_BDATAL);
+    return wireReadData16Bit(BDATA);
 }
 
 uint8_t APDS9960::readProximity() {
-    return wireReadData8Bit(APDS9960_PDATA);
+    return wireReadData8Bit(PDATA);
 }
 
 uint8_t APDS9960::getProxIntLowThresh() {
     /* Read value from PILT register */
-    return wireReadData8Bit(APDS9960_PILT);
+    return wireReadData8Bit(PILT);
 }
 
 void APDS9960::setProxIntLowThresh(uint8_t threshold) {
-    wireWriteData8Bit(APDS9960_PILT, threshold);
+    wireWriteData8Bit(PILT, threshold);
 }
 
 uint8_t APDS9960::getProxIntHighThresh() {
-    return wireReadData8Bit(APDS9960_PIHT);
+    return wireReadData8Bit(PIHT);
 }
 
 void APDS9960::setProxIntHighThresh(uint8_t threshold) {
-    wireWriteData8Bit(APDS9960_PIHT, threshold);
+    wireWriteData8Bit(PIHT, threshold);
 }
 
 uint8_t APDS9960::getLEDDrive() {
     /* Shift and mask out LED drive bits */
-    return (wireReadData8Bit(APDS9960_CONTROL) >> 6) & 0b00000011;
+    return (wireReadData8Bit(CONTROL) >> 6) & 0b00000011;
 }
 
-void APDS9960::setLEDDrive(uint8_t drive) {
-    uint8_t val = wireReadData8Bit(APDS9960_CONTROL);
+void APDS9960::setLEDDrive(LEDDrive drive) {
+    uint8_t val = wireReadData8Bit(CONTROL);
     /* Set bits in register to given value */
-    drive &= 0b00000011;
-    drive = drive << 6;
     val &= 0b00111111;
-    val |= drive;
-    wireWriteData8Bit(APDS9960_CONTROL, val);
+    val |= (drive & 0b00000011) << 6;
+    wireWriteData8Bit(CONTROL, val);
 }
 
 uint8_t APDS9960::getProximityGain() {
     /* Shift and mask out PDRIVE bits */
-    return (wireReadData8Bit(APDS9960_CONTROL) >> 2) & 0b00000011;
+    return (wireReadData8Bit(CONTROL) >> 2) & 0b00000011;
 }
 
-void APDS9960::setProximityGain(uint8_t drive) {
-    uint8_t val = wireReadData8Bit(APDS9960_CONTROL);
+void APDS9960::setProximityGain(ProximityGain gain) {
+    uint8_t val = wireReadData8Bit(CONTROL);
     /* Set bits in register to given value */
-    drive &= 0b00000011;
-    drive = drive << 2;
     val &= 0b11110011;
-    val |= drive;
-    wireWriteData8Bit(APDS9960_CONTROL, val);
+    val |= (gain & 0b00000011) << 2;
+    wireWriteData8Bit(CONTROL, val);
 }
 
 uint8_t APDS9960::getAmbientLightGain() {
     /* Shift and mask out ADRIVE bits */
-    return wireReadData8Bit(APDS9960_CONTROL) & 0b00000011;
+    return wireReadData8Bit(CONTROL) & 0b00000011;
 }
 
-void APDS9960::setAmbientLightGain(uint8_t drive) {
-    uint8_t val = wireReadData8Bit(APDS9960_CONTROL);
+void APDS9960::setAmbientLightGain(ALSGain gain) {
+    uint8_t val = wireReadData8Bit(CONTROL);
     /* Set bits in register to given value */
-    drive &= 0b00000011;
     val &= 0b11111100;
-    val |= drive;
-    wireWriteData8Bit(APDS9960_CONTROL, val);
+    val |= gain & 0b00000011;
+    wireWriteData8Bit(CONTROL, val);
 }
 
 uint8_t APDS9960::getLEDBoost() {
-    uint8_t val = wireReadData8Bit(APDS9960_CONFIG2);
+    uint8_t val = wireReadData8Bit(CONFIG2);
     /* Shift and mask out LED_BOOST bits */
     return (val >> 4) & 0b00000011;
 }
 
 void APDS9960::setLEDBoost(uint8_t boost) {
-    uint8_t val = wireReadData8Bit(APDS9960_CONFIG2);
+    uint8_t val = wireReadData8Bit(CONFIG2);
     /* Set bits in register to given value */
     boost &= 0b00000011;
     boost = boost << 4;
     val &= 0b11001111;
     val |= boost;
-    wireWriteData8Bit(APDS9960_CONFIG2, val);
+    wireWriteData8Bit(CONFIG2, val);
 }
 
 uint8_t APDS9960::getProxGainCompEnable() {
     /* Shift and mask out PCMP bits */
-    return (wireReadData8Bit(APDS9960_CONFIG3) >> 5) & 0b00000001;
+    return (wireReadData8Bit(CONFIG3) >> 5) & 0b00000001;
 }
 
 void APDS9960::setProxGainCompEnable(uint8_t enable) {
-    uint8_t val = wireReadData8Bit(APDS9960_CONFIG3);
+    uint8_t val = wireReadData8Bit(CONFIG3);
     /* Set bits in register to given value */
     enable &= 0b00000001;
     enable = enable << 5;
     val &= 0b11011111;
     val |= enable;
-    wireWriteData8Bit(APDS9960_CONFIG3, val);
+    wireWriteData8Bit(CONFIG3, val);
 }
 
 uint8_t APDS9960::getProxPhotoMask() {
     /* Mask out photodiode enable mask bits */
-    return wireReadData8Bit(APDS9960_CONFIG3) & 0b00001111;;
+    return wireReadData8Bit(CONFIG3) & 0b00001111;;
 }
 
 void APDS9960::setProxPhotoMask(uint8_t mask) {
-    uint8_t val = wireReadData8Bit(APDS9960_CONFIG3);
+    uint8_t val = wireReadData8Bit(CONFIG3);
     /* Set bits in register to given value */
     mask &= 0b00001111;
     val &= 0b11110000;
     val |= mask;
-    wireWriteData8Bit(APDS9960_CONFIG3, val);
+    wireWriteData8Bit(CONFIG3, val);
 }
 
 uint16_t APDS9960::getLightIntLowThreshold() {
-    return wireReadData16Bit(APDS9960_AILTL);
+    return wireReadData16Bit(AILT);
 }
 
 void APDS9960::setLightIntLowThreshold(uint16_t threshold) {
-    wireWriteData16Bit(APDS9960_AILTL, threshold);
+    wireWriteData16Bit(AILT, threshold);
 }
 
 uint16_t APDS9960::getLightIntHighThreshold() {
-    return wireReadData16Bit(APDS9960_AIHTL);
+    return wireReadData16Bit(AIHT);
 }
 
 void APDS9960::setLightIntHighThreshold(uint16_t threshold) {
-    wireWriteData16Bit(APDS9960_AIHTL, threshold);
+    wireWriteData16Bit(AIHT, threshold);
 }
 
 uint8_t APDS9960::getProximityIntLowThreshold() {
-    return wireReadData8Bit(APDS9960_PILT);
+    return wireReadData8Bit(PILT);
 }
 
 void APDS9960::setProximityIntLowThreshold(uint8_t threshold) {
-    wireWriteData8Bit(APDS9960_PILT, threshold);
+    wireWriteData8Bit(PILT, threshold);
 }
 
 uint8_t APDS9960::getProximityIntHighThreshold() {
-    return wireReadData8Bit(APDS9960_PIHT);
+    return wireReadData8Bit(PIHT);
 }
 
 void APDS9960::setProximityIntHighThreshold(uint8_t threshold) {
-    wireWriteData8Bit(APDS9960_PIHT, threshold);
+    wireWriteData8Bit(PIHT, threshold);
 }
 
 uint8_t APDS9960::getAmbientLightIntEnable() {
     /* Shift and mask out AIEN bit */
-    return (wireReadData8Bit(APDS9960_ENABLE) >> 4) & 0b00000001;
+    return (wireReadData8Bit(ENABLE) >> 4) & 0b00000001;
 }
 
 void APDS9960::setAmbientLightIntEnable(uint8_t enable) {
-    uint8_t val = wireReadData8Bit(APDS9960_ENABLE);
+    uint8_t val = wireReadData8Bit(ENABLE);
     /* Set bits in register to given value */
     enable &= 0b00000001;
     enable = enable << 4;
     val &= 0b11101111;
     val |= enable;
-    wireWriteData8Bit(APDS9960_ENABLE, val);
+    wireWriteData8Bit(ENABLE, val);
 }
 
 uint8_t APDS9960::getProximityIntEnable() {
     /* Shift and mask out PIEN bit */
-    return (wireReadData8Bit(APDS9960_ENABLE) >> 5) & 0b00000001;
+    return (wireReadData8Bit(ENABLE) >> 5) & 0b00000001;
 }
 
 void APDS9960::setProximityIntEnable(uint8_t enable) {
     uint8_t val;
-    val = wireReadData8Bit(APDS9960_ENABLE);
+    val = wireReadData8Bit(ENABLE);
     /* Set bits in register to given value */
     enable &= 0b00000001;
     enable = enable << 5;
     val &= 0b11011111;
     val |= enable;
-    wireWriteData8Bit(APDS9960_ENABLE, val);
+    wireWriteData8Bit(ENABLE, val);
 }
 
 void APDS9960::clearAmbientLightInt() {
-    wireReadData8Bit(APDS9960_AICLEAR);
+    wireReadData8Bit(AICLEAR);
 }
 
 void APDS9960::clearProximityInt() {
-    wireReadData8Bit(APDS9960_PICLEAR);
+    wireReadData8Bit(PICLEAR);
 }
 
 void APDS9960::wireWrite8Bit(uint8_t val) {
--- RobotMain/APDS9960.h
+++ RobotMain/APDS9960.h
@@ -7,82 +7,12 @@
 /* APDS-9960 I2C address */
 #define APDS9960_I2C_ADDR       0x39
 
-/* Error code for returned values */
-#define ERROR                   0xFF
-
 /* Acceptable device IDs */
 #define APDS9960_ID_1           0xAB
 #define APDS9960_ID_2           0x9C
 
 /* Misc parameters */
 #define FIFO_PAUSE_TIME         30      // Wait period (ms) between FIFO reads
-
-/* APDS-9960 register addresses */
-#define APDS9960_ENABLE         0x80
-#define APDS9960_ATIME          0x81
-#define APDS9960_WTIME          0x83
-#define APDS9960_AILTL          0x84
-#define APDS9960_AILTH          0x85
-#define APDS9960_AIHTL          0x86
-#define APDS9960_AIHTH          0x87
-#define APDS9960_PILT           0x89
-#define APDS9960_PIHT           0x8B
-#define APDS9960_PERS           0x8C
-#define APDS9960_CONFIG1        0x8D
-#define APDS9960_PPULSE         0x8E
-#define APDS9960_CONTROL        0x8F
-#define APDS9960_CONFIG2        0x90
-#define APDS9960_ID             0x92
-#define APDS9960_STATUS         0x93
-#define APDS9960_CDATAL         0x94
-#define APDS9960_CDATAH         0x95
-#define APDS9960_RDATAL         0x96
-#define APDS9960_RDATAH         0x97
-#define APDS9960_GDATAL         0x98
-#define APDS9960_GDATAH         0x99
-#define APDS9960_BDATAL         0x9A
-#define APDS9960_BDATAH         0x9B
-#define APDS9960_PDATA          0x9C
-#define APDS9960_POFFSET_UR     0x9D
-#define APDS9960_POFFSET_DL     0x9E
-#define APDS9960_CONFIG3        0x9F
-#define APDS9960_IFORCE         0xE4
-#define APDS9960_PICLEAR        0xE5
-#define APDS9960_CICLEAR        0xE6
-#define APDS9960_AICLEAR        0xE7
-
-/* Acceptable parameters for setMode */
-#define POWER                   0
-#define AMBIENT_LIGHT           1
-#define PROXIMITY               2
-#define WAIT                    3
-#define AMBIENT_LIGHT_INT       4
-#define PROXIMITY_INT           5
-#define ALL                     7
-
-/* LED Drive values */
-#define LED_DRIVE_100MA         0
-#define LED_DRIVE_50MA          1
-#define LED_DRIVE_25MA          2
-#define LED_DRIVE_12_5MA        3
-
-/* Proximity Gain (PGAIN) values */
-#define PGAIN_1X                0
-#define PGAIN_2X                1
-#define PGAIN_4X                2
-#define PGAIN_8X                3
-
-/* ALS Gain (AGAIN) values */
-#define AGAIN_1X                0
-#define AGAIN_4X                1
-#define AGAIN_16X               2
-#define AGAIN_64X               3
-
-/* LED Boost values */
-#define LED_BOOST_100           0
-#define LED_BOOST_150           1
-#define LED_BOOST_200           2
-#define LED_BOOST_300           3
 
 /* Default values */
 #define DEFAULT_ATIME           219     // 103ms
@@ -91,9 +21,6 @@
 #define DEFAULT_POFFSET_UR      0       // 0 offset
 #define DEFAULT_POFFSET_DL      0       // 0 offset
 #define DEFAULT_CONFIG1         0x60    // No 12x wait (WTIME) factor
-#define DEFAULT_LDRIVE          LED_DRIVE_100MA
-#define DEFAULT_PGAIN           PGAIN_4X
-#define DEFAULT_AGAIN           AGAIN_4X
 #define DEFAULT_PILT            0       // Low proximity threshold
 #define DEFAULT_PIHT            50      // High proximity threshold
 #define DEFAULT_AILT            0xFFFF  // Force interrupt for calibration
@@ -105,12 +32,64 @@
 /* APDS9960 Class */
 class APDS9960 {
 public:
+  /* Acceptable parameters for setMode */
+  enum Mode : uint8_t {
+    POWER, AMBIENT_LIGHT, PROXIMITY, WAIT, AMBIENT_LIGHT_INT, PROXIMITY_INT, ALL
+  };
+  /* LED Drive values */
+  enum LEDDrive : uint8_t {
+    LD100MA, LD50MA, LD25MA, LD12_5MA
+  };
+  /* Proximity Gain (PGAIN) values */
+  enum ProximityGain : uint8_t {
+    PG1X, PG2X, PG4X, PG8X
+  };
+  /* ALS Gain (AGAIN) values */
+  enum ALSGain : uint8_t {
+    AG1X, AG4X, AG16X, AG64X
+  };
+  /* LED Boost values */
+  enum LEDBoost : uint8_t {
+    LB100, LB150, LB200, LB300
+  };
 
+private:
+    /* APDS-9960 register addresses */
+    enum Register : uint8_t {
+      ENABLE = 0x80,
+      ATIME = 0x81,
+      WTIME = 0x83,
+      AILT = 0x84,
+      AIHT = 0x86,
+      PILT = 0x89,
+      PIHT = 0x8B,
+      PERS = 0x8C,
+      CONFIG1 = 0x8D,
+      PPULSE = 0x8E,
+      CONTROL = 0x8F,
+      CONFIG2 = 0x90,
+      ID = 0x92,
+      STATUS = 0x93,
+      CDATA = 0x94,
+      RDATA = 0x96,
+      GDATA = 0x98,
+      BDATA = 0x9A,
+      PDATA = 0x9C,
+      POFFSET_UR = 0x9D,
+      POFFSET_DL = 0x9E,
+      CONFIG3 = 0x9F,
+      IFORCE = 0xE4,
+      PICLEAR = 0xE5,
+      CICLEAR = 0xE6,
+      AICLEAR = 0xE7
+    };
+
+public:
     /* Initialization methods */
     void begin();
     bool check();
     uint8_t getMode();
-    void setMode(uint8_t mode, uint8_t enable);
+    void setMode(Mode mode, uint8_t enable);
 
     /* Turn the APDS-9960 on and off */
     void enablePower();
@@ -124,13 +103,13 @@ public:
 
     /* LED drive strength control */
     uint8_t getLEDDrive();
-    void setLEDDrive(uint8_t drive);
+    void setLEDDrive(LEDDrive drive);
 
     /* Gain control */
     uint8_t getAmbientLightGain();
-    void setAmbientLightGain(uint8_t gain);
+    void setAmbientLightGain(ALSGain gain);
     uint8_t getProximityGain();
-    void setProximityGain(uint8_t gain);
+    void setProximityGain(ProximityGain gain);
 
     /* Get and set light interrupt thresholds */
     uint16_t getLightIntLowThreshold();
@@ -164,7 +143,6 @@ public:
     uint8_t readProximity();
 
 private:
-
     /* Proximity Interrupt Threshold */
     uint8_t getProxIntLowThresh();
     void setProxIntLowThresh(uint8_t threshold);
